420c55d1db1777dba4275f6e8bb6125114d5ff1c
Charis-Opol <charisopol@gmail.com>
2025-01-18 21:19:49 +0300

    Tenth update

--- dsa_arrays.py
+++ dsa_arrays.py
@@ -59,3 +59,33 @@ for i in range (n):
             my_array1[j], my_array1[min_index] = my_array1[min_index], my_array1[j]
 
 print("Sorted array: ", my_array1)
+
+#Insertion Sort algorithm
+my_array1 = [64, 34, 25, 12, 22, 11, 90, 5]
+
+n = len(my_array1)
+for i in range (1, n): #This is because the algorithm does not compare the fist value it only compares the second value and the rest
+    insert_index = i
+    current_value = my_array1.pop(i)
+    for j in range (i-1, -1, -1):
+        if current_value < my_array1[j]: #If  the current value being compared is smaller than the value in the sorted array then it is inserted in the place of that value
+            insert_index = j
+    my_array1.insert(insert_index, current_value)
+
+print("Sorted array:", my_array1)
+
+#Improved Insertion Sort Algorithm. Where we shift and make space for the value rather than poping
+my_array1 = [64, 34, 25, 12, 22, 11, 90, 5]
+
+n = len(my_array1)
+for i in range (1, n):
+    insert_index = i
+    current_value = my_array1[i]
+    for j in range (i-1, -1, -1):
+        if current_value < my_array1[j]:
+            my_array1[j+1] = my_array1[j] # In this step the value in the array is shifted
+            insert_index = j
+        else:
+            break
+    my_array1[insert_index] = current_value
+print("Sorted array:", my_array1)
