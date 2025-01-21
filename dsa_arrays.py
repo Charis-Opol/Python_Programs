24488283621237d106893b831a8b21705f80b43d
Charis-Opol <charisopol@gmail.com>
2025-01-21 12:09:34 +0300

    Twelfth update

--- dsa_arrays.py
+++ dsa_arrays.py
@@ -110,4 +110,38 @@ my_array1 = [64, 34, 25, 12, 22, 11, 90, 5]
 print("Sorted array:", quick_sort(my_array1))
 
 
+#Merge sort algorithm
+
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    
+    mid = len(array) // 2
+    leftHalf = array[:mid]
+    rightHalf = array[mid:]
+
+    sortedLeft = merge_sort(leftHalf)
+    sortedRight = merge_sort(rightHalf)
+
+    return merge(sortedLeft, sortedRight)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
+
+my_array1 = [64, 34, 25, 12, 22, 11, 90, 5]
+print("Sorted array:", merge_sort(my_array1))
+
     
