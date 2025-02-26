3e03b52597e78a4b6cdf3465e9b41d8149ff7823
Charis-Opol <charisopol@gmail.com>
2025-02-26 21:26:21 +0300

    Changes

--- groupwork.py
+++ groupwork.py
@@ -1,12 +1,12 @@
-def insertion_sort(array):
+def insertion_sort(array): # Function for insertion sort
     n = len(array)
-    for i in range (1, n):
-        insert_index = i
-        current_value = array.pop(insert_index)
-        for j in range (i-1, -1, -1):
-            if array[j] > current_value:
-                insert_index = j
-        array.insert(insert_index, current_value) 
+    for i in range (1, n): # Loop through the array starting from the second element
+        insert_index = i # Initialize the insert index to the current element's position
+        current_value = array.pop(insert_index) # Remove the current element from the array and store it in a variable
+        for j in range (i-1, -1, -1): # Loop through the elements before the current element in reverse order
+            if array[j] > current_value: # If the current element is smaller than the element before it
+                insert_index = j # Move the insert index to the position before the current element
+        array.insert(insert_index, current_value) # Insert the current element at the correct position
     return array
 
 
